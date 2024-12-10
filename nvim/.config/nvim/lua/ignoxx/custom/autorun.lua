398c64342c8ba0a40c67a4249ffd9a868afad448
ignas <ignas.kavaliauskas@axelspringer.com>
2024-12-10 11:48:39 +0100

    fix autorun cmd

--- nvim/.config/nvim/lua/ignoxx/custom/autorun.lua
+++ nvim/.config/nvim/lua/ignoxx/custom/autorun.lua
@@ -1,6 +1,4 @@
 local attach_to_buffer = function(buf, pattern, cmd)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "**output:**" })
-
 	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 		group = vim.api.nvim_create_augroup("mawife", { clear = true }),
 		pattern = pattern,
@@ -13,14 +11,15 @@ local attach_to_buffer = function(buf, pattern, cmd)
 						end
 					end
 
-					vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+					vim.api.nvim_buf_set_lines(buf, 0, -1, false, data)
 				end
 			end
 
 			vim.fn.jobstart(cmd, {
 				stdout_buffered = true,
 				on_stdout = append_data,
-				on_stderr = append_data,
+				-- stderr_buffered = true,
+				-- on_stderr = append_data,
 			})
 		end,
 	})
