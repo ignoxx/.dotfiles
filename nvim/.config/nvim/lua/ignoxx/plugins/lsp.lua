eafe24090fb1a4ce94edc9b2ad9a0656bbaf9d04
ignoxx <ignoxx@yahoo.de>
2025-04-28 14:22:59 +0200

    update: aerospace

--- nvim/.config/nvim/lua/ignoxx/plugins/lsp.lua
+++ nvim/.config/nvim/lua/ignoxx/plugins/lsp.lua
@@ -33,8 +33,8 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
-				"rust_analyzer",
 				"gopls",
+				"ts_ls",
 			},
 			handlers = {
 				function(server_name) -- default handler (optional)
